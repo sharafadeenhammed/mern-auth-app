ccf3972cea74af70ab4add81a92fb36d6639f9fa
Sharafadeen Hammed <sharafadeenhammed@yahoo.com>
2023-05-25 17:50:34 +0100

    mongo db setup user model created and body parser mounted

--- backend/server.js
+++ backend/server.js
@@ -10,13 +10,24 @@ const PORT = process.env.NODE_PORT || 5000;
 //import router logics...
 import userRoutes from "./routes/userRoutes.js";
 import { errorHandeler, notFound } from "./midddleware/errorHandeler.js";
+import connectDb from "./config/db.js";
 
 const app = express();
+
+//connet to database...
+connectDb();
+
+// mounting morgan middleware for developement...
 app.use(morgan("dev"));
+
+// mount body parsers...
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/v1/auth", userRoutes);
 
 app.use(notFound, errorHandeler);
+
 app.listen(PORT, () => {
   console.log(`app runnning on port ${PORT}`.blue.underline.inverse);
 });
